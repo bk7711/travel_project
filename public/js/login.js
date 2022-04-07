e0942b3f2b78ec4ed88e3387ff59662991f83cf3
Carolyn B <bmathematics@gmail.com>
2022-04-06 21:07:32 -0400

    create models, public files, and seeds

---
+++ public/js/login.js
@@ -0,0 +1,48 @@
+//user login
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+//sign up user
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-sign").value.trim();
+  const email = document.querySelector("#email-sign").value.trim();
+  const password = document.querySelector("#password-sign").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,13 @@
+const seedUsers = require("./user-seeds");
+const sequelize = require("../config/connection");
 
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("--------------");
+  await seedUsers();
+  console.log("--------------");
+
+  process.exit(0);
+};
+
+seedAll();
