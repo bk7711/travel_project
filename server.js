fd2496e22305f82c55eb2ec5422dbfd5d2178770
christanfox <bayshorestinks@aol.com>
2022-04-09 16:28:42 -0400

    homepage now live

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// const path = require('path');
+const path = require('path');
 const express = require("express");
 const routes = require("./controller");
 const sequelize = require("./config/connection");
-// const exphbs = require("express-handlebars");
-// const helpers = require('./utils/helpers');
+const exphbs = require("express-handlebars");
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
