390f01683416dc93403d4f8190ce77c130b00d91
christanfox <bayshorestinks@aol.com>
2022-04-11 19:48:02 -0400

    working on login page

--- Models/index.js
+++ Models/index.js
@@ -5,14 +5,42 @@ const Hotel = require("./Hotels");
 const Restaurant = require("./Restaurants");
 const Comment = require("./Comment");
 const Trip = require("./Trip");
+
 // create associations
-
-// user has ...
-
-// Post belongsTo
-
-// User belongsToMany
-
-//
+Trip.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+User.hasMany(Trip, {
+  //   through: "Trip",
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+Hotel.belongsTo(Location, {
+  foreignKey: "location_id",
+});
+// Hotel.belongsToMany(Trip, {
+//   through: "Trip",
+//   foreignKey: "id",
+// });
+// Location.belongsToMany(Trip, {
+//   through: "Trip",
+//   foreignKey: "id",
+// });
+Restaurant.belongsTo(Location, {
+  foreignKey: "location_id",
+});
+// Restaurant.belongsToMany(Trip, {
+//   through: "Trip",
+//   foreignKey: "id",
+// });
+Comment.belongsTo(User, {
+  through: "Trip",
+  foreignKey: "comment_id",
+});
 
 module.exports = { User, Location, Hotel, Restaurant, Comment, Trip };
--- controller/api/index.js
+++ controller/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require("./user-routes.js");
 const locationRoutes = require("./location-routes.js");
 const hotelRoutes = require("./hotel-routes.js");
 const restaurantRoutes = require("./restaurant-routes.js");
+const tripRoutes = require("./trip-routes");
 
 router.use("/users", userRoutes);
 router.use("/locations", locationRoutes);
 router.use("/hotels", hotelRoutes);
 router.use("/restaurants", restaurantRoutes);
+router.use("/trips", tripRoutes);
 
 module.exports = router;
