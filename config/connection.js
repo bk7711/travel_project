c1faf6023e7b3eacceb0a20a3addf3d2fd9ad8f3
unknown <avrumiesafranovitz@gmail.com>
2022-04-12 20:14:58 -0400

    added config/connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-<<<<<<< HEAD
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
@@ -14,12 +13,5 @@ const sequelize = process.env.JAWSDB_URL
         port: 3306,
       }
     );
-=======
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
->>>>>>> 9b9d7ebc1f3455f8dea9c5c20d0164011cbe06b7
 
 module.exports = sequelize;
