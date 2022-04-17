ea2051f64b94f628745da7a46a2c6e9de67d4557
njacques47 <nojacques94@gmail.com>
2022-04-17 10:32:50 -0400

    broken changes to surrounding login

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,14 @@
 // since these values were repeated I made them "global" to this sheet (hopefully)
-const username = document.querySelector(".username-signup").value.trim();
-const email = document.querySelector(".email-signup").value.trim();
-const password = document.querySelector(".password-signup").value.trim();
+// const username = document.querySelector(".username-signup").value.trim();
+// const email = document.querySelector(".email-signup").value.trim();
+// const password = document.querySelector(".password-signup").value.trim();
 
 // handles user login form
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // const email = document.querySelector(".email-login").value.trim();
-  // const password = document.querySelector(".password-login").value.trim();
+  const email = document.querySelector(".email-login").value.trim();
+  const password = document.querySelector(".password-login").value.trim();
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
@@ -27,12 +27,35 @@ const loginFormHandler = async (event) => {
 };
 
 // handles user input for sign up
+// async function signupFormHandler(event) {
+//   event.preventDefault();
+
+//   const username = document.querySelector(".username-signup").value.trim();
+//   const email = document.querySelector(".email-signup").value.trim();
+//   const password = document.querySelector(".password-signup").value.trim();
+
+//   if (username && email && password) {
+//     const response = await fetch("/api/users", {
+//       method: "POST",
+//       body: JSON.stringify({ username, email, password }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
+
+// tester area where I changed the redirect location to the dashboard after signing up
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  // const username = document.querySelector(".username-signup").value.trim();
-  // const email = document.querySelector(".email-signup").value.trim();
-  // const password = document.querySelector(".password-signup").value.trim();
+  const username = document.querySelector(".username-signup").value.trim();
+  const email = document.querySelector(".email-signup").value.trim();
+  const password = document.querySelector(".password-signup").value.trim();
 
   if (username && email && password) {
     const response = await fetch("/api/users", {
@@ -42,17 +65,32 @@ async function signupFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
   }
 }
 
+// querySelector didn't feel like the right choice here because its looking for a <p>.
+// when I hover over querySelector, it says "querySelector<Element>: Element(+2 overload)"....idk what that means lol
+// Login by selecting btn_login
+// should the documents be moved inside of their respective functions? I think so because then the "default action" would have something to target and wait for.
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
 
+  // Logout
 document
   .querySelector(".signup-form")
   .addEventListener("submit", signupFormHandler);
+
+
+//   document
+//   .getElementsByClassName(".login-form")
+//   .addEventListener("submit", loginFormHandler);
+
+//   // Logout
+// document
+//   .getElementsByClassName(".signup-form")
+//   .addEventListener("submit", signupFormHandler);
--- public/js/style.js
+++ public/js/style.js
@@ -12,3 +12,4 @@ signUpBtn.addEventListener("click", () => {
 });
 fistForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
